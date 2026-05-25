32bb524a970623f4434acc7e8e8ec150f87157c6
princekori2324-spec <princekori2324@gmail.com>
2026-05-25 22:59:55 +0530

    first commit

---
+++ Project/main.js
@@ -0,0 +1,23 @@
+// change navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className = "fa-solid fa-minus"
+        }else{
+            icon.className = "fa-solid fa-plus"
+        }
+    });
+})
